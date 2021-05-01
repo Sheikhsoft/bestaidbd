1e3a67c2e63c45342767a3935fd761e053bfceb3
skshamimul <skshamimul@gmail.com>
2021-05-02 02:23:36 +0600

    update slider

--- front-page.php
+++ front-page.php
@@ -4,7 +4,7 @@
 	<div class="hero-slider">
 
 		<!-- Start Single Slider -->
-		<div class="single-slider" style="background-image:url('http://localhost/bestaid/wp-content/uploads/2021/04/slider2.jpg')">
+		<div class="single-slider" style="background-image:url('http://bestaidbd.sheikhsoft.com/wp-content/uploads/2021/05/slider2.jpg')">
 			<div class="container">
 				<div class="row">
 					<div class="col-lg-7">
@@ -23,7 +23,7 @@
 		</div>
 		<!-- Start End Slider -->
 		<!-- Start Single Slider -->
-		<div class="single-slider" style="background-image:url('http://localhost/bestaid/wp-content/uploads/2021/04/slider.jpg')">
+		<div class="single-slider" style="background-image:url('http://bestaidbd.sheikhsoft.com/wp-content/uploads/2021/05/slider.jpg')">
 			<div class="container">
 				<div class="row">
 					<div class="col-lg-7">
--- header.php
+++ header.php
@@ -100,7 +100,7 @@
 									}
 									?>
 
-									
+
 								</nav>
 							</div>
 							<!--/ End Main Menu -->
